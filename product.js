bf8fd6fe24acfb420439b82863bc2eaea20408de
benoitD03 <bens.92@hotmail.fr>
2021-01-14 18:36:23 +0100

    affichage page produit ok

---
+++ product.js
@@ -0,0 +1,87 @@
+let productImage = document.getElementById("imgProduct");
+let productTitle = document.getElementById("titleProduct");
+let productDescription = document.getElementById("descriptionProduct");
+let productPrice = document.getElementById("priceProduct");
+
+let dropDownList = document.getElementById("dropDownList");
+
+let urlParams = new URLSearchParams(window.location.search);
+let id = urlParams.get('id');
+const apiUrlId = `http://localhost:3000/api/teddies?id=${id}`;
+
+// Afficher la page produit en fonction de l'ourson choisi
+function displayProduct(data) {
+    
+    switch (id) {
+        case data[0]._id : 
+            productImage.src = data[0].imageUrl;
+            productTitle.innerHTML = data[0].name;
+            productDescription.innerHTML = data[0].description;
+            productPrice.innerHTML = data[0].price / 100 + " €";
+            for(let i = 0; i < data[0].colors.length; i++){
+                let colors = document.createElement("option");
+                dropDownList.appendChild(colors);
+                colors.innerHTML = data[0].colors[i];
+            }
+            break;
+        case data[1]._id : 
+            productImage.src = data[1].imageUrl;
+            productTitle.innerHTML = data[1].name;
+            productDescription.innerHTML = data[1].description;
+            productPrice.innerHTML = data[1].price / 100 + " €";
+            for(let i = 0; i < data[1].colors.length; i++){
+                let colors = document.createElement("option");
+                dropDownList.appendChild(colors);
+                colors.innerHTML = data[1].colors[i];
+            }
+            break;
+        case data[2]._id : 
+            productImage.src = data[2].imageUrl;
+            productTitle.innerHTML = data[2].name;
+            productDescription.innerHTML = data[2].description;
+            productPrice.innerHTML = data[2].price / 100 + " €";
+            for(let i = 0; i < data[2].colors.length; i++){
+                let colors = document.createElement("option");
+                dropDownList.appendChild(colors);
+                colors.innerHTML = data[2].colors[i];
+            }
+            break;
+        case data[3]._id : 
+            productImage.src = data[3].imageUrl;
+            productTitle.innerHTML = data[3].name;
+            productDescription.innerHTML = data[3].description;
+            productPrice.innerHTML = data[3].price / 100 + " €";
+            for(let i = 0; i < data[3].colors.length; i++){
+                let colors = document.createElement("option");
+                dropDownList.appendChild(colors);
+                colors.innerHTML = data[3].colors[i];
+            }
+            break;
+        case data[4]._id : 
+            productImage.src = data[4].imageUrl;
+            productTitle.innerHTML = data[4].name;
+            productDescription.innerHTML = data[4].description;
+            productPrice.innerHTML = data[4].price / 100 + " €";
+            for(let i = 0; i < data[4].colors.length; i++){
+                let colors = document.createElement("option");
+                dropDownList.appendChild(colors);
+                colors.innerHTML = data[4].colors[i];
+            }
+            break;
+    }
+  
+}
+
+
+
+fetch(apiUrlId)
+    .then(response => {
+        if(response.ok){
+            response.json().then(data => {
+                displayProduct(data);
+            })
+        } else {
+            console.log("erreur");
+            }
+        
+    });
--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+// PAGE D'ACCUEIL
 const apiUrl = "http://localhost:3000/api/teddies";
-let bearImg = document.getElementsByClassName("card-img");
-let bearName = document.getElementsByClassName("card-title");
-let bearPrice = document.getElementsByClassName("card-text");
+
+let row = document.getElementById("teddies");
+
+
 
 // Compléter chaque cartes de la liste des ours en peluche
 function completeProductList(data) {
+
     for(let i = 0; i < data.length; i++) {
-        bearImg[i].src = data[i].imageUrl;
-        bearName[i].innerHTML = data[i].name;
-        bearPrice[i].innerHTML = data[i].price + " €";
+        
+
+        let col = document.createElement("div");
+        col.classList.add("col-12", "col-md-3", "carte");
+        row.appendChild(col);
+
+        let linkCard = document.createElement("a");
+        linkCard.href = "produit.html?id=" + data[i]._id;
+        col.appendChild(linkCard);
+
+        // let card = document.createElement("div");
+        // card.classList.add("card");
+        // linkCard.appendChild("card");
+
+        let bearImg = document.createElement("img");
+        bearImg.src = data[i].imageUrl;
+        bearImg.classList.add("card-img");
+        linkCard.appendChild(bearImg);
+
+        let bearName = document.createElement("h3");
+        bearName.innerText = data[i].name;
+        linkCard.appendChild(bearName);
+
+        let bearPrice = document.createElement("p");
+        bearPrice.innerText = data[i].price / 100 + " €";
+        linkCard.appendChild(bearPrice);
+    
     }
 }
+
 
 fetch(apiUrl)
     .then(response => {
@@ -24,3 +52,16 @@ fetch(apiUrl)
             }
         }
     });
+
+
+{/* <div class="col-12 col-md-3">
+    <a href="produit.html" id="link2" class="liens">
+        <div class="card">
+            <img src="" alt="" class="card-img">
+            <div class="card-body">
+                <h3 class="card-title text-center"></h3>
+                <p class="card-text text-center"></p>
+            </div>
+        </div>
+    </a>
+</div> */}
